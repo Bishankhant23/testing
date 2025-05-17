15a587d02636733af6952e72144e5f6e2fc746eb
bishanpatel123 <bishanpatel123@gmail.com>
2025-05-17 21:28:44 +0530

    testing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express  = require("express")
-
+const path = require("path")
 const app = express()
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
 
 app.get("/",(req,res) => {
     return res.send("workinggg")
+})
+
+app.get("/name/:name",(req,res) => {
+    const {name} = req.params;
+    return res.render("index",{name})
 })
 
 app.listen(3000,() => {
